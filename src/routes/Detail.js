06af02a722273595859210b8f593bf7f7f688b50
Gohana <opo7726@gmail.com>
2022-09-27 13:44:00 +0900

    Update Detail.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,30 +1,16 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-
-function Detail({ coverImg, title, year, summary, genres }) {
-   const { id } = useParams();
-   const getMovie = async () => {
-      const json = await(
-         await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-         ).json();
-         console.log(json);
-   };
-   useEffect(() => {
-      getMovie();
-   }, [])
-   return (
-   <div>
-      <img src={coverImg} alt={title}/>
-      <div>
-        <p>{summary}</p>
-        <ul>
-          {genres.map((g) => (
-            <li key={g}>{g}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-   );
+function Detail() {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return <h1>Detail</h1>;
 }
-
 export default Detail;
